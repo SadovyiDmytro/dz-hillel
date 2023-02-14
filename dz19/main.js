d9e714d472c0e9e38fa92eb6115dcd0fec281942
SadovyiDmytro <dmitry.w2pro@gmail.com>
2023-02-14 18:49:34 +0200

    next

--- dz19/main.js
+++ dz19/main.js
@@ -1,1 +1,42 @@
-const btn = document.querySelector('btnLink');
+const firstBtnLink = document.querySelector('.one-btn');
+const secBtnLink = document.querySelector('.two-btn');
+
+
+let majorLink
+firstBtnLink.addEventListener('click', () => {
+ 
+    if (firstBtnLink.dataset.link.includes('https://') === false || firstBtnLink.dataset.link.includes('https://') === false) {
+        alert('ERROR: Need to type a protocol')
+        majorLinkInt = prompt("Type: http or https protocol you need?");
+        majorLink = firstBtnLink.dataset.link.constructor(majorLinkInt + '://' + firstBtnLink.dataset.link);
+        if (majorLink.includes('https://') === true || majorLink.includes('http://') === true){
+        console.log('Go to:' + majorLink + '?');
+        }
+        else {
+            alert('ERROR: Not available protocol')
+        }
+    }
+    else {
+        console.log(firstBtnLink.dataset.link)
+    }
+});
+
+let secondaryLink
+secBtnLink.addEventListener('click', () => { 
+ 
+    if (secBtnLink.dataset.link.includes('https://') === true) {
+        console.log('Clearly go to:' + secBtnLink.dataset.link + '?')
+
+    }
+    else {
+        alert('ERROR: Need to type a protocol')
+        secondLinkInt = prompt("Type: http or https protocol you need?");
+        secondaryLink = firstBtnLink.dataset.link.constructor(secondLinkInt + '://' + firstBtnLink.dataset.link);
+        if (secondaryLink.includes('https://') === true || secondaryLink.includes('http://') === true){
+        console.log('Go to:' + secondaryLink + '?');
+        }
+        else {
+            alert('ERROR: Not available protocol')
+        }
+    }
+});
